82f75c92e25a8ce7d6841e20d9eaebb6b0b323aa
lydiachen01 <137934459+lydiachen01@users.noreply.github.com>
2024-02-18 11:32:24 -0500

    Update three.tsx

--- app/(drawer)/(tabs)/three.tsx
+++ app/(drawer)/(tabs)/three.tsx
@@ -1,12 +1,20 @@
-import { YStack, H2, Separator, Theme } from 'tamagui';
+import { YStack, H2, Theme, Paragraph, Text, Button } from 'tamagui';
 
-import EditScreenInfo from '../../../components/edit-screen-info';
-
-export default function TabThreeScreen() {
+export default function enableNotifications() {
   return (
-    <Theme name="light">
-      <YStack flex={1} alignItems="center" justifyContent="center">
-        <H2>This is the resources page</H2>
+    <Theme name="dark">
+      <Logo></Logo>
+
+      <YStack backgroundColor={'$blue1'} flex={1} alignItems="center" justifyContent="center">
+        <H2 color="white">Get Important Updates</H2>
+        <Paragraph textAlign='center' maxWidth={313} paddingTop={40} paddingBottom={60}>Stay connected and never miss an update!
+          Turn on notifications to receive important alerts about voter
+          registration deadlines, polling location changes, wait times,
+          and more. Keep informed and engaged with MyVoterInfo!"
+        </Paragraph>
+
+        <Button borderRadius={5} backgroundColor={'$blue4'}>Turn Notifications On</Button>
+        <Button width={160} chromeless>Maybe Later</Button>
       </YStack>
     </Theme>
   );
